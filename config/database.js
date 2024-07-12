d1b30eda42db9dc26409655bf54e2493074ca38e
interfacetuner <damianchankins@gmail.com>
2024-07-12 14:17:15 -0400

    edit routes to messages page

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'MongoDB secret', {
-useNewUrlParser: true,
-useCreateIndex: true,
-useUnifiedTopology: true,
-useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://SEI:Weezer@cluster0.7peh7.mongodb.net/messages?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+  });
 
 const db = mongoose.connection;
 // Connect to MongoDB
